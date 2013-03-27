515eae476d0ff60d17ab41e934b9b01e00355fe0
seike takumi <seike.takumi@tis.co.jp>
2013-03-27 10:57:21 +0900

    deliver server time to clients

--- app.js
+++ app.js
@@ -5,23 +5,24 @@ var options = {
     };
 var app = require('express')()
   , server = require('https').createServer(options, app)
-  , io = require('socket.io').listen(server);
+  , io = require('socket.io').listen(server, {'log level': 3});
 
 server.listen(443);
 
 app.get('/', function (req, res) {
   res.sendfile(__dirname + '/index.html');
 });
-
+app.get('/index.js', function(req, res){
+  res.sendfile(__dirname + '/index.js');
+});
 io.sockets.on('connection', function (socket) {
   socket.emit('message', { message: 'welcome!!'});
   socket.emit('update', { date: Date().toString() });
 
   socket.on('update', function(){
-    console.log("hey hey hey");
     setTimeout(function(){
       socket.emit('update', { date: Date().toString() });
-    }, 3000);
+    }, 1000);
   });
 });
 
