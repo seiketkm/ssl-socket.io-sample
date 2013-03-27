c3d120e35d767387e44d9e70e0170875976e8732
seike takumi <seike.takumi@tis.co.jp>
2013-03-27 17:30:31 +0900

    push more

---
+++ index.js
@@ -0,0 +1,30 @@
+$(function(){
+  var socket = io.connect('//localhost');
+  socket.on('update', function (data) {
+    console.log(data.date);
+    socket.emit('update', {});
+    $('body').trigger('socketio_update', data.date);
+  });
+  socket.on('message', function(data){
+    console.log(data.message);
+  });
+
+  var ListView = Backbone.View.extend({
+    el: $('body'),
+
+    events: {
+      'socketio_update': 'addItem'
+    },
+    initialize: function(){
+      _.bindAll(this, 'render', 'addItem');
+      this.render();
+    },
+    render: function(){
+      $(this.el).append("<ul></ul>");
+    },
+    addItem: function(e,data){
+      $('ul', this.el).append("<li>" + data + "</li>");
+    }
+  });
+  var listView = new ListView();
+});
